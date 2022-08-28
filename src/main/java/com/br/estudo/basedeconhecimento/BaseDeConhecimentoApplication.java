166ea9a5df65b4afc6466d73c62158dff396be39
DESKTOP-6D4ABJL\JHONATHAN <jhonathandosreis@hotmail.com>
2022-08-28 00:17:03 -0300

    Projeto Inicial Criado.

---
+++ src/main/java/com/br/estudo/basedeconhecimento/BaseDeConhecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.br.estudo.basedeconhecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeConhecimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseDeConhecimentoApplication.class, args);
+    }
+
+}
